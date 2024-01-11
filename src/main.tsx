3ee47bd9801b8c6c643c445c1b6b28bfbd49ec90
Sarthak Agarwal <sarthakroxxx99@gmail.com>
2024-01-12 01:24:34 +0530

    updated react query config

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,19 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthContextProvider } from "./hooks/useAuth.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
